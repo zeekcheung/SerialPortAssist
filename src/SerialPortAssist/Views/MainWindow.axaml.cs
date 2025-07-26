3ac645e5d410070c34c6ae056ff547ca9faa5f4e
zeekcheung <zeekcheung@outlook.com>
2025-07-26 11:59:31 +0800

    feat: 创建界面

--- src/SerialPortAssist/Views/MainWindow.axaml.cs
+++ src/SerialPortAssist/Views/MainWindow.axaml.cs
@@ -1,12 +1,19 @@
-using Avalonia.Controls;
+using System;
+using Avalonia.Interactivity;
+using Ursa.Controls;
 
 namespace SerialPortAssist.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : UrsaWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenRepository(object? sender, RoutedEventArgs e)
+        {
+            Launcher.LaunchUriAsync(new Uri("https://github.com/zeekcheung/SerialPortAssist"));
         }
     }
 }
