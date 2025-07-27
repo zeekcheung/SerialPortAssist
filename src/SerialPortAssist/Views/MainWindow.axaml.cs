be2acb9a94765c9450ff22072d1e39549bd3f293
zeekcheung <zeekcheung@outlook.com>
2025-07-27 21:48:22 +0800

    feat: 收发数据

--- src/SerialPortAssist/Views/MainWindow.axaml.cs
+++ src/SerialPortAssist/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Interactivity;
+using SerialPortAssist.ViewModels;
 using Ursa.Controls;
 
 namespace SerialPortAssist.Views
@@ -14,6 +15,17 @@ namespace SerialPortAssist.Views
         private void OpenRepository(object? sender, RoutedEventArgs e)
         {
             Launcher.LaunchUriAsync(new Uri("https://github.com/zeekcheung/SerialPortAssist"));
+        }
+
+        /// <summary>
+        /// 发送区文本框失去焦点时，自动将文本框内容添加到发送缓冲区，并启动自动发送数据定时器
+        /// </summary>
+        private void OnTxtSendDataLostFocus(object? sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.HandleTxtSendDataLostFocus(sender, e);
+            }
         }
     }
 }
