zeekcheung/SerialPortHelper

feat: 协议解析

SerialPortHelper/Converters/BytesToHexStringConverter.cs
@@ -1,0 +1,30 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using SerialPortHelper.Helpers;
+
+namespace SerialPortHelper.Converters
+{
+    public class BytesToHexStringConverter : IValueConverter
+    {
+        public object? Convert(
+            object? value,
+            Type targetType,
+            object? parameter,
+            CultureInfo culture
+        )
+        {
+            return value is byte[] bytes ? bytes.ToHexString() : null;
+        }
+
+        public object? ConvertBack(
+            object? value,
+            Type targetType,
+            object? parameter,
+            CultureInfo culture
+        )
+        {
+            return value is string hexString ? hexString.ToBytes() : null;
+        }
+    }
+}

SerialPortHelper/DataCheckers/Crc8.cs
@@ -1,0 +1,49 @@
+using System;
+
+namespace SerialPortHelper.DataCheckers
+{
+    public class Crc8 : IDataChecker
+    {
+        /// <summary>
+        /// 生成多项式
+        /// </summary>
+        private const byte Poly = 0x07;
+
+        /// <summary>
+        /// 初始值
+        /// </summary>
+        private const byte Init = 0x00;
+
+        private static readonly byte[] s_table = BuildTable();
+
+        private static byte[] BuildTable()
+        {
+            var t = new byte[256];
+            for (var i = 0; i < 256; i++)
+            {
+                var crc = (byte)i;
+                for (var j = 0; j < 8; j++)
+                {
+                    crc = (crc & 0x80) != 0 ? (byte)((crc << 1) ^ Poly) : (byte)(crc << 1);
+                    t[i] = crc;
+                }
+            }
+            return t;
+        }
+
+        public static byte Compute(ReadOnlySpan<byte> data)
+        {
+            var crc = Init;
+            foreach (var b in data)
+            {
+                crc = s_table[crc ^ b];
+            }
+            return crc;
+        }
+
+        public bool Check(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

SerialPortHelper/DataParsers/SampleDataParser.cs
@@ -1,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SerialPortHelper.DataParsers
+{
+    /// <summary>
+    /// 示例数据解析器
+    /// 数据格式：帧头（0x7F）+ 数据长度（1字节）+ 数据（N字节）+ 校验码（1字节/CRC8）
+    /// </summary>
+    public class SampleDataParser : IDataParser
+    {
+        /// <summary>
+        /// 帧头
+        /// </summary>
+        private const byte FrameHeader = 0x7F;
+
+        /// <summary>
+        /// 最小帧长度：帧头（1字节）+ 数据长度（1字节）+ 校验码（2字节）
+        /// </summary>
+        private const int MinFrameLength = 4;
+
+        /// <summary>
+        /// 数据队列
+        /// </summary>
+        private readonly Queue<byte> _queue = new();
+
+        /// <summary>
+        /// 迭代器函数，循环解析所有数据帧
+        /// </summary>
+        /// <param name="buffer">数据缓冲区</param>
+        /// <returns>数据帧迭代器</returns>
+        public IEnumerable<byte[]> GetDataFrames(byte[] buffer)
+        {
+            // 所有字节入队
+            foreach (var b in buffer)
+            {
+                _queue.Enqueue(b);
+            }
+
+            // 解析所有数据帧
+            while (true)
+            {
+                // 找帧头
+                while (_queue.Count > 0 && _queue.Peek() != FrameHeader)
+                {
+                    _queue.Dequeue();
+                }
+
+                // 判断是否有最小帧，确保数据长度和校验码字段存在
+                if (_queue.Count < MinFrameLength)
+                {
+                    // 终止整个迭代器，跳出循环
+                    yield break;
+                }
+
+                // 读取数据长度，确定数据帧长度
+                var payloadLength = _queue.ElementAt(1);
+                var frameLength = MinFrameLength + payloadLength;
+
+                // 判断是否有足够的数据，确保数据帧完整
+                if (_queue.Count < frameLength)
+                {
+                    // 终止整个迭代器，跳出循环
+                    yield break;
+                }
+
+                // 读取实际数据，组装成数据帧
+                var frame = new byte[frameLength];
+                for (var i = 0; i < frameLength; i++)
+                {
+                    frame[i] = _queue.Dequeue();
+                }
+
+                // 返回数据帧，交出控制权
+                yield return frame;
+            }
+        }
+    }
+}

SerialPortHelper/Protocols/SampleProtocol.cs
@@ -1,0 +1,26 @@
+using SerialPortHelper.DataCheckers;
+using SerialPortHelper.DataParsers;
+
+namespace SerialPortHelper.Protocols
+{
+    public class SampleProtocol : IProtocol
+    {
+        public IDataParser Parser { get; set; } = new SampleDataParser();
+        public IDataChecker Checker { get; set; } = new Crc16Checker();
+
+        public void ParseAndCheck(byte[] buffer, FrameHandler handler)
+        {
+            // 提取所有数据帧
+            var frames = Parser.GetDataFrames(buffer);
+
+            // 提取每个帧的实际数据并进行数据校验
+            foreach (var frame in frames)
+            {
+                // 校验数据
+                var isValid = Checker.Check(frame);
+
+                handler?.Invoke(frame, isValid);
+            }
+        }
+    }
+}

zeekcheung/SerialPortAssist

src/SerialPortAssist/Converters/BytesToHexStringConverter.cs
@@ -1,0 +1,30 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using SerialPortAssist.Helpers;
+
+namespace SerialPortAssist.Converters
+{
+    public class BytesToHexStringConverter : IValueConverter
+    {
+        public object? Convert(
+            object? value,
+            Type targetType,
+            object? parameter,
+            CultureInfo culture
+        )
+        {
+            return value is byte[] bytes ? bytes.ToHexString() : null;
+        }
+
+        public object? ConvertBack(
+            object? value,
+            Type targetType,
+            object? parameter,
+            CultureInfo culture
+        )
+        {
+            return value is string hexString ? hexString.ToBytes() : null;
+        }
+    }
+}

src/SerialPortAssist/DataCheckers/Crc8.cs
@@ -1,0 +1,49 @@
+using System;
+
+namespace SerialPortAssist.DataCheckers
+{
+    public class Crc8 : IDataChecker
+    {
+        /// <summary>
+        /// 生成多项式
+        /// </summary>
+        private const byte Poly = 0x07;
+
+        /// <summary>
+        /// 初始值
+        /// </summary>
+        private const byte Init = 0x00;
+
+        private static readonly byte[] s_table = BuildTable();
+
+        private static byte[] BuildTable()
+        {
+            var t = new byte[256];
+            for (var i = 0; i < 256; i++)
+            {
+                var crc = (byte)i;
+                for (var j = 0; j < 8; j++)
+                {
+                    crc = (crc & 0x80) != 0 ? (byte)((crc << 1) ^ Poly) : (byte)(crc << 1);
+                    t[i] = crc;
+                }
+            }
+            return t;
+        }
+
+        public static byte Compute(ReadOnlySpan<byte> data)
+        {
+            var crc = Init;
+            foreach (var b in data)
+            {
+                crc = s_table[crc ^ b];
+            }
+            return crc;
+        }
+
+        public bool Check(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

src/SerialPortAssist/DataParsers/SampleDataParser.cs
@@ -1,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SerialPortAssist.DataParsers
+{
+    /// <summary>
+    /// 示例数据解析器
+    /// 数据格式：帧头（0x7F）+ 数据长度（1字节）+ 数据（N字节）+ 校验码（1字节/CRC8）
+    /// </summary>
+    public class SampleDataParser : IDataParser
+    {
+        /// <summary>
+        /// 帧头
+        /// </summary>
+        private const byte FrameHeader = 0x7F;
+
+        /// <summary>
+        /// 最小帧长度：帧头（1字节）+ 数据长度（1字节）+ 校验码（2字节）
+        /// </summary>
+        private const int MinFrameLength = 4;
+
+        /// <summary>
+        /// 数据队列
+        /// </summary>
+        private readonly Queue<byte> _queue = new();
+
+        /// <summary>
+        /// 迭代器函数，循环解析所有数据帧
+        /// </summary>
+        /// <param name="buffer">数据缓冲区</param>
+        /// <returns>数据帧迭代器</returns>
+        public IEnumerable<byte[]> GetDataFrames(byte[] buffer)
+        {
+            // 所有字节入队
+            foreach (var b in buffer)
+            {
+                _queue.Enqueue(b);
+            }
+
+            // 解析所有数据帧
+            while (true)
+            {
+                // 找帧头
+                while (_queue.Count > 0 && _queue.Peek() != FrameHeader)
+                {
+                    _queue.Dequeue();
+                }
+
+                // 判断是否有最小帧，确保数据长度和校验码字段存在
+                if (_queue.Count < MinFrameLength)
+                {
+                    // 终止整个迭代器，跳出循环
+                    yield break;
+                }
+
+                // 读取数据长度，确定数据帧长度
+                var payloadLength = _queue.ElementAt(1);
+                var frameLength = MinFrameLength + payloadLength;
+
+                // 判断是否有足够的数据，确保数据帧完整
+                if (_queue.Count < frameLength)
+                {
+                    // 终止整个迭代器，跳出循环
+                    yield break;
+                }
+
+                // 读取实际数据，组装成数据帧
+                var frame = new byte[frameLength];
+                for (var i = 0; i < frameLength; i++)
+                {
+                    frame[i] = _queue.Dequeue();
+                }
+
+                // 返回数据帧，交出控制权
+                yield return frame;
+            }
+        }
+    }
+}

src/SerialPortAssist/Protocols/SampleProtocol.cs
@@ -1,0 +1,26 @@
+using SerialPortAssist.DataCheckers;
+using SerialPortAssist.DataParsers;
+
+namespace SerialPortAssist.Protocols
+{
+    public class SampleProtocol : IProtocol
+    {
+        public IDataParser Parser { get; set; } = new SampleDataParser();
+        public IDataChecker Checker { get; set; } = new Crc16Checker();
+
+        public void ParseAndCheck(byte[] buffer, FrameHandler handler)
+        {
+            // 提取所有数据帧
+            var frames = Parser.GetDataFrames(buffer);
+
+            // 提取每个帧的实际数据并进行数据校验
+            foreach (var frame in frames)
+            {
+                // 校验数据
+                var isValid = Checker.Check(frame);
+
+                handler?.Invoke(frame, isValid);
+            }
+        }
+    }
+}