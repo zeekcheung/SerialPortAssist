zeekcheung/SerialPortHelper

chore: 初始化项目

SerialPortHelper/ViewModels/ViewModelBase.cs
@@ -1,0 +1,6 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SerialPortHelper.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject { }
+}

SerialPortHelper/Views/MainWindow.axaml.cs
@@ -1,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SerialPortHelper.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

zeekcheung/SerialPortAssist

src/SerialPortAssist/ViewModels/ViewModelBase.cs
@@ -1,0 +1,6 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SerialPortAssist.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject { }
+}

src/SerialPortAssist/Views/MainWindow.axaml.cs
@@ -1,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SerialPortAssist.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}